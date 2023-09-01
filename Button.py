f1caaa141be850cce17665f7c5f89f4deacde068
lwimber39 <lwimber39@charlottesvilleschools.org>
2023-09-01 15:21:28 -0400

    Second button added

--- Button.py
+++ Button.py
@@ -6,11 +6,22 @@ btn = DigitalInOut(board.D8)
 btn.direction = Direction.INPUT
 btn.pull = Pull.DOWN
 
+btn2 = DigitalInOut(board.D9)
+btn2.direction = Direction.INPUT
+btn2.pull = Pull.DOWN
+
 while True:
     if btn.value:
         print("BTN is down")
     else:
         print("BTN is up")
         pass
+
+    time.sleep(0.1) # sleep for debounce
+    if btn2.value:
+        print("BTN2 is down")
+    else:
+        print("BTN2 is up")
+    pass
 
     time.sleep(0.1) # sleep for debounce
