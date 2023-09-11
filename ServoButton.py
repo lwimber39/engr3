444ff222381bb8328e6886ff253c9ba2b54a3d1a
lwimber39 <lwimber39@charlottesvilleschools.org>
2023-09-11 15:29:15 -0400

    Got rid of comments

--- ServoButton.py
+++ ServoButton.py
@@ -1,5 +1,3 @@
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
 
 import time
 import board
@@ -7,7 +5,6 @@ import pwmio
 from adafruit_motor import servo
 from digitalio import DigitalInOut, Direction, Pull
 
-# create a PWMOut object on the control pin.
 pwm = pwmio.PWMOut(board.D5, duty_cycle=0, frequency=50)
 
 btn = DigitalInOut(board.D8)
@@ -18,28 +15,9 @@ btn2 = DigitalInOut(board.D9)
 btn2.direction = Direction.INPUT 
 btn2.pull = Pull.DOWN
 
-# To get the full range of the servo you will likely need to adjust the min_pulse and max_pulse to
-# match the stall points of the servo.
-# This is an example for the Sub-micro servo: https://www.adafruit.com/product/2201
-# servo = servo.Servo(pwm, min_pulse=580, max_pulse=2350)
-# This is an example for the Micro Servo - High Powered, High Torque Metal Gear:
-#   https://www.adafruit.com/product/2307
-# servo = servo.Servo(pwm, min_pulse=500, max_pulse=2600)
-# This is an example for the Standard servo - TowerPro SG-5010 - 5010:
-#   https://www.adafruit.com/product/155
-# servo = servo.Servo(pwm, min_pulse=400, max_pulse=2400)
-# This is an example for the Analog Feedback Servo: https://www.adafruit.com/product/1404
-# servo = servo.Servo(pwm, min_pulse=600, max_pulse=2500)
-# This is an example for the Micro servo - TowerPro SG-92R: https://www.adafruit.com/product/169
-# servo = servo.Servo(pwm, min_pulse=500, max_pulse=2400)
-
-# The pulse range is 750 - 2250 by default. This range typically gives 135 degrees of
-# range, but the default is to use 180 degrees. You can specify the expected range if you wish:
-# servo = servo.Servo(board.D5, actuation_range=135)
 servo = servo.Servo(pwm)
 angle = 90
 
-# We sleep in the loops to give the servo time to move into position.
 while True:
     if btn.value:
         angle = angle + 2
