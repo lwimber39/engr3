a883e7a762a951f0a4481d494b6f3b5923dec2ff
lwimber39 <lwimber39@charlottesvilleschools.org>
2023-10-17 15:21:08 -0400

    eggaraafgaa

--- DistanceSensorNeopixel.py
+++ DistanceSensorNeopixel.py
@@ -4,12 +4,20 @@
 import time
 import board
 import adafruit_hcsr04
+import neopixel
 
 sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D5, echo_pin=board.D6)
 
 while True:
     try:
-        print((sonar.distance,))
+        print((sonar.distance))
+        dist = sonar.distance
+        if dist < 5: 
+            pass
+            #light up LED somehow
+        elif dist > 35:
+            pass
+            #light up LED somehow
     except RuntimeError:
         print("Retrying!")
-    time.sleep(0.1)
+        time.sleep(0.1)    
