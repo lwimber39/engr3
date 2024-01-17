cd8c8228b90cb759089938f37f7e8d10b33a4db1
lwimber39 <lwimber39@charlottesvilleschools.org>
2024-01-17 15:55:08 -0500

    kyaegrtewatg

---
+++ LCDScan.py
@@ -0,0 +1,13 @@
+import board
+import busio
+import time
+
+i2c = busio.I2C(board.SCL, board.SDA)
+
+while not i2c.try_lock():
+    pass
+
+while True:
+    print("I2C addresses found:", [hex(device_address)
+                                   for device_address in i2c.scan()])
+    time.sleep(2)
