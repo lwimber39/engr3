3b768fadd983cc0815660b4e899129292e58d13f
lwimber39 <lwimber39@charlottesvilleschools.org>
2024-01-25 15:46:49 -0500

    waryaryara

--- Photointerrupter.py
+++ Photointerrupter.py
@@ -2,12 +2,26 @@ import time
 import digitalio
 from lcd.lcd import LCD
 from lcd.i2c_pcf8574_interface import I2CPCF8574Interface
+import board
 
 photointerrupter = digitalio.DigitalInOut(board.D2)
 photointerrupter.direction = digitalio.Direction.INPUT
 photointerrupter.pull = digitalio.Pull.UP
 photointerrupter_state = None
-if not photointerrupter.value and photointerrupter_state is None:
-photointerrupter_state = "interrupted"
+interrupt_counter = 0
+
+lcd = LCD(I2CPCF8574Interface(board.I2C(), 0x27), num_rows=2, num_cols=16)
 lcd.set_cursor_pos(0,0)
 lcd.print("The number of interrupts is: ")
+now = time.monotonic()
+
+while True:
+    if not photointerrupter.value and photointerrupter_state is None:
+        photointerrupter_state = "interrupted"
+    if photointerrupter.value and photointerrupter_state == "interrupted":
+        photointerrupter_state = None
+        interrupt_counter = interrupt_counter+1
+    if (now + 4) < time.monotonic():
+        lcd.set_cursor_pos(1,13)
+        lcd.print(str(interrupt_counter))
+        now = time.monotonic()
