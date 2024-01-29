3466085f2565cdb2783f9fff2a36c764d98ef5da
lwimber39 <lwimber39@charlottesvilleschools.org>
2024-01-29 15:51:30 -0500

    tylt5rkjyhy

--- StepperMotot.py
+++ StepperMotot.py
@@ -14,3 +14,27 @@ coils = (
     digitalio.DigitalInOut(board.D11), # B1
     digitalio.DigitalInOut(board.D12), # B2
 )
+
+for coil in coils:
+    coil.direction = digitalio.Direction.OUTPUT
+
+motor = stepper.StepperMotor(coils[0], coils[1], coils[2], coils[3], microsteps=None)
+
+for step in range(STEPS):
+    motor.onestep(style=stepper.DOUBLE)
+    time.sleep(DELAY)
+
+for step in range(STEPS):
+    motor.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
+    time.sleep(DELAY)
+
+async def catch_pin_transitions(pin):
+    with keypad.Keys((pin,), value_when_pressed=False) as keys:
+        while True:
+            event = keys.events.get()
+            if event:
+                if event.pressed:
+                    print("Limit Switch was pressed.")
+                elif event.released:
+                    print("Limit Switch was released.")
+            await asyncio.sleep(0)
