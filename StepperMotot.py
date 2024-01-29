a63cf91ac8ee189df6a14b0d4ed9ca086b631b40
lwimber39 <lwimber39@charlottesvilleschools.org>
2024-01-29 15:43:21 -0500

    ethehteh

---
+++ StepperMotot.py
@@ -0,0 +1,16 @@
+import asyncio
+import board
+import keypad
+import time
+import digitalio
+from adafruit_motor import stepper
+
+DELAY = 0.01
+STEPS = 100
+
+coils = (
+    digitalio.DigitalInOut(board.D9),  # A1
+    digitalio.DigitalInOut(board.D10), # A2
+    digitalio.DigitalInOut(board.D11), # B1
+    digitalio.DigitalInOut(board.D12), # B2
+)
